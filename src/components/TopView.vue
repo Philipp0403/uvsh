d96536a1c5057838509abd47813a8b3497047aab
Philipp0403 <philipp.uesbeck@student.uni-luebeck.de>
2024-01-18 15:36:49 +0100

    First CSS Shit

---
+++ src/components/TopView.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <h1>Vergleich Universitäten Schleswig-Holstein</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'TopView',
+    }
+</script>
+
+<style scoped>
+header {
+    background-color: #9719eb;
+    height: 12rem;
+    padding: 2rem;
+    display: flex;
+    align-items: flex-end;
+}
+</style>
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,3 +1,13 @@
 <template>
     <h1>HomePage</h1>
+    <top-view></top-view>
 </template>
+
+<script>
+import TopView from '@/components/TopView.vue'
+export default {
+  components: {
+    TopView
+  }
+}
+</script>
