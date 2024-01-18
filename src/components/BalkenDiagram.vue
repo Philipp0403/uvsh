6343bfcdec79cbd61163f3f40117a46dd67291d7
Philipp0403 <philipp.uesbeck@student.uni-luebeck.de>
2024-01-18 20:56:08 +0100

    Diagram laeuft

--- src/components/BalkenDiagram.vue
+++ src/components/BalkenDiagram.vue
@@ -1,13 +1,34 @@
 <template>
     <div>
-        <p></p>
+        <Bar
+            id="my-chart-id"
+            :options="chartOptions"
+            :data="chartData"
+        />
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'BalkenDiagram',
+import { Bar } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+    name: 'BalkenDiagram',
+    components: { Bar },
+    data() {
+        return {
+            chartData: {
+                labels: ['A', 'B'],
+                datasets: [ {data: [40, 20]}]
+            },
+            chartOptions: {
+                responsive: true
+            }
+        }
     }
+}
 </script>
 
 <style scoped>
---
+++ src/data/balken.js
@@ -0,0 +1,23 @@
+export const balkenData = () => ({
+    labels: [
+        'UzL',
+        'CAU',
+        'EUF'
+    ],
+    datasets: [
+        {
+            label: 'Daten halt',
+            backgroundColor: '#f87979',
+            data: [
+                30,
+                20,
+                10
+            ]
+        }
+    ]
+})
+
+export const options = {
+    responsive: true,
+    maintainAspectRatio: false
+}
