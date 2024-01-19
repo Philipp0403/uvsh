9517021aeb2db8765de65d6c93ebcb8cb329171d
Akasha Kessel <akasha.kessel@student.uni-luebeck.de>
2024-01-19 13:41:36 +0100

    updated diagram

--- src/components/BalkenDiagram.vue
+++ src/components/BalkenDiagram.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <div @click="balken = !balken">Zeitverlauf</div>
     <div class="dropdown">
       <div>
         <div id="currentYear" @click="dropDownOpen = !dropDownOpen">
@@ -17,13 +18,19 @@
       </div>
     </div>
     <div>
-      <Bar id="my-chart-id" :options="options" :data="data" />
+      <Bar
+        v-if="balken"
+        id="my-chart-id"
+        :options="options"
+        :data="balkenData"
+      />
+      <Line v-if="!balken" :options="options" :data="balkenData" />
     </div>
   </div>
 </template>
 
 <script>
-import { Bar } from "vue-chartjs";
+import { Bar, Line } from "vue-chartjs";
 import {
   Chart as ChartJS,
   Title,
@@ -32,8 +39,10 @@ import {
   BarElement,
   CategoryScale,
   LinearScale,
+  PointElement,
+  LineElement,
 } from "chart.js";
-import * as balken from "../data/balken.js";
+import { uzlEinnahmen, eufEinnahmen } from "../data/alldata.js";
 
 ChartJS.register(
   Title,
@@ -41,21 +50,142 @@ ChartJS.register(
   Legend,
   BarElement,
   CategoryScale,
-  LinearScale
+  LinearScale,
+  PointElement,
+  LineElement
 );
 
 export default {
   name: "BalkenDiagram",
-  components: { Bar },
+  components: { Bar, Line },
   data() {
     return {
       jahr: 2011,
       jahre: [2011, 2012, 2013, 2014, 2015, 2016, 2017],
       dropDownOpen: false,
-      ...balken,
+      balken: false,
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
     };
   },
   computed: {
+    balkenData() {
+      return {
+        labels: ["UzL", "CAU", "EUF"],
+        datasets: [
+          {
+            label: "Landeszuschuss zu Betriebskosten",
+            backgroundColor: "#4AD66D",
+            data: [uzlEinnahmen[this.$store.state.jahr], 20, 10],
+          },
+          {
+            label: "Zuschüsse zur Finanzierung von Investitionen",
+            backgroundColor: "#25A244",
+            data: [30, 20, 10],
+          },
+          {
+            label: "Drittmittel",
+            backgroundColor: "#1A7431",
+            data: [30, 20, 80],
+          },
+          {
+            label: "Sonstige Einnahmen",
+            backgroundColor: "#00A6FB",
+            data: [30, 20, 10],
+          },
+          {
+            label: "Nicht genauer spezifizierte Einnahmen",
+            backgroundColor: "#006494",
+            data: [30, 20, 10],
+          },
+          {
+            label: "Gesamt",
+            backgroundColor: "#003554",
+            data: [0, 20, 80],
+          },
+          {
+            label: "Sachausgaben",
+            backgroundColor: "#E87D2A",
+            data: [30, 20, 10],
+          },
+          {
+            label: "Personalausgaben",
+            backgroundColor: "#DE6D17",
+            data: [30, 20, 80],
+          },
+          {
+            label: "Nicht genauer spezifizierte Ausgaben",
+            backgroundColor: "#EF2B2B",
+            data: [30, 20, 10],
+          },
+          {
+            label: "Gesamtsumme",
+            backgroundColor: "#CD0000",
+            data: [30, 20, 10],
+          },
+        ],
+      };
+    },
+    lineData() {
+      return {
+        labels: ["2011", "2012", "2013", "2014", "2015", "2016", "2017"],
+        datasets: [
+          {
+            label: "Landeszuschuss zu Betriebskosten",
+            backgroundColor: "#4AD66D",
+            uzldata: uzlEinnahmen,
+            eufdata: eufEinnahmen,
+          },
+          {
+            label: "Zuschüsse zur Finanzierung von Investitionen",
+            backgroundColor: "#25A244",
+            data: [30, 20, 10],
+          },
+          {
+            label: "Drittmittel",
+            backgroundColor: "#1A7431",
+            data: [30, 20, 80],
+          },
+          {
+            label: "Sonstige Einnahmen",
+            backgroundColor: "#00A6FB",
+            data: [30, 20, 10],
+          },
+          {
+            label: "Nicht genauer spezifizierte Einnahmen",
+            backgroundColor: "#006494",
+            data: [30, 20, 10],
+          },
+          {
+            label: "Gesamt",
+            backgroundColor: "#003554",
+            data: [0, 20, 80],
+          },
+          {
+            label: "Sachausgaben",
+            backgroundColor: "#E87D2A",
+            data: [30, 20, 10],
+          },
+          {
+            label: "Personalausgaben",
+            backgroundColor: "#DE6D17",
+            data: [30, 20, 80],
+          },
+          {
+            label: "Nicht genauer spezifizierte Ausgaben",
+            backgroundColor: "#EF2B2B",
+            data: [30, 20, 10],
+          },
+          {
+            label: "Gesamtsumme",
+            backgroundColor: "#CD0000",
+            data: [30, 20, 10],
+          },
+        ],
+      };
+    },
     andereJahre() {
       return this.jahre.filter((anderesJahr) => anderesJahr != this.jahr);
     },
